c1ffa8bd74de204b2e233ace17514dbb2f4d39b1
Grégory <fulgueiras.gregory@gmail.com>
2026-05-26 20:48:17 +0200

    feat: add task creation and display on dashboard

--- src/main/java/com/yrog/famiplan/controller/DashboardController.java
+++ src/main/java/com/yrog/famiplan/controller/DashboardController.java
@@ -1,13 +1,40 @@
 package com.yrog.famiplan.controller;
 
+import com.yrog.famiplan.entity.Task;
+import com.yrog.famiplan.repository.TaskRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/dashboard")
 public class DashboardController {
 
-    @GetMapping("/dashboard")
-    public String getDashboard () {
+    private final TaskRepository taskRepository;
+    public DashboardController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping
+    public String getDashboard (Model model) {
+        model.addAttribute("tasks", taskRepository.findAll());
+        model.addAttribute("task", new Task());
         return "dashboard";
+    }
+
+    @PostMapping
+    public String createTask(@Valid @ModelAttribute Task task, BindingResult result) {
+        if (result.hasErrors()) {
+            return "dashboard";
+        }
+        taskRepository.save(task);
+
+        return "redirect:/dashboard";
+
     }
 }
--- src/main/java/com/yrog/famiplan/entity/Task.java
+++ src/main/java/com/yrog/famiplan/entity/Task.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -22,7 +23,7 @@ public class Task {
     private Long id;
 
     @Setter
-    @NotNull
+    @NotBlank
     private String description;
 
     @NotNull
